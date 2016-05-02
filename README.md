6c3933fbdf4029b4857960e979e0c123f22dcdff
Piotr Plonski <piotr.plonski@iqor.com>
2016-05-02 12:20:07 +0200

    add python script to dump model into simple text file

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # keras2cpp
 This is a bunch of code to port Keras neural network model into pure C++.
 
-    model.to_json()
+## Usage
+1. Save your Keras model architecture and weights:
+    with open('my_nn_arch.json', 'w') as fout:
+        fout.write(model.to_json())
+    model.save_weights('my_nn_weights.h5')
+2. Run converter, that will put architecture and weights into text file:
+    python dump_to_simple_cpp.py -a my_nn_arch.json -w my_nn_weights.h5 -o nn_output.data
+3. Use text file with network definition with KerasModel.cpp and KerasModel.h files. 
 
-a
---
+++ dump_to_simple_cpp.py
@@ -0,0 +1,63 @@
+import numpy as np
+np.random.seed(1337)
+from keras.models import Sequential, model_from_json
+import json
+import argparse
+
+parser = argparse.ArgumentParser(description='This is a simple script to dump Keras model into simple format suitable for porting into pure C++ model')
+
+parser.add_argument('-a', '--architecture', help="JSON with model architecture", required=True)
+parser.add_argument('-w', '--weights', help="Model weights in HDF5 format", required=True)
+parser.add_argument('-o', '--output', help="Ouput file name", required=True)
+
+args = parser.parse_args()
+
+print 'Read architecture from', args.architecture
+print 'Read weights from', args.weights
+print 'Writing to', args.output
+
+
+arch = open(args.architecture).read()
+model = model_from_json(arch)
+model.load_weights(args.weights)
+model.compile(loss='categorical_crossentropy', optimizer='adadelta')
+arch = json.loads(arch)
+
+with open(args.output, 'w') as fout:
+    fout.write('layers ' + str(len(model.layers)) + '\n')
+    
+    layers = []
+    for ind, l in enumerate(arch["config"]):
+        fout.write('layer ' + str(ind) + ' ' + l['class_name'] + '\n')
+    
+        print str(ind), l['class_name'] 
+        layers += [l['class_name']]
+        if l['class_name'] == 'Convolution2D':
+            fout.write(str(l['config']['nb_filter']) + ' ' + str(l['config']['nb_col']) + ' ' + str(l['config']['nb_row']) + ' ')
+        
+            if 'batch_input_shape' in l['config']:
+                fout.write(str(l['config']['batch_input_shape'][2]) + ' ' + str(l['config']['batch_input_shape'][3])) 
+            fout.write('\n')
+            
+            W = model.layers[ind].get_weights()[0]
+            
+            for i in range(W.shape[0]):
+                for j in range(W.shape[1]):
+                    for k in range(W.shape[2]):
+                        fout.write(str(W[i,j,k]) + '\n')
+            fout.write(str(model.layers[ind].get_weights()[1]) + '\n')
+            
+        if l['class_name'] == 'Activation':
+            fout.write(l['config']['activation'] + '\n')
+        if l['class_name'] == 'MaxPooling2D':
+            fout.write(str(l['config']['pool_size'][0]) + ' ' + str(l['config']['pool_size'][1]) + '\n')
+        #if l['class_name'] == 'Flatten':
+        #    print l['config']['name']
+        if l['class_name'] == 'Dense':
+            fout.write(str(l['config']['output_dim']) + '\n')
+            W = model.layers[ind].get_weights()[0]
+            
+            for w in W:
+                fout.write(str(w) + '\n')
+            fout.write(str(model.layers[ind].get_weights()[1]) + '\n')
+
