18ec88f29c01f29c534d2fed65f147fe25f30b97
pp <pplonski86@gmail.com>
2016-11-22 16:34:57 +0100

    Run example with border_mode=same

--- example/mnist_cnn_one_iteration.py
+++ example/mnist_cnn_one_iteration.py
@@ -46,10 +46,10 @@ Y_test = np_utils.to_categorical(y_test, nb_classes)
 
 model = Sequential()
 
-model.add(Convolution2D(nb_filters, nb_conv, nb_conv, border_mode='valid',
+model.add(Convolution2D(nb_filters, nb_conv, nb_conv, border_mode='same',
                         input_shape=(1, img_rows, img_cols)))
 model.add(Activation('relu'))
-model.add(Convolution2D(nb_filters, nb_conv, nb_conv))
+model.add(Convolution2D(nb_filters, nb_conv, nb_conv, border_mode='same'))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(nb_pool, nb_pool)))
 model.add(Dropout(0.25))
@@ -84,6 +84,5 @@ with open("./sample_mnist.dat", "w") as fin:
 print('Prediction on saved sample:')
 print(str(model.predict(X_train[:1])))
 # on my pc I got:
-#[[ 0.01091413  0.00714095  0.06447848  0.39298642  0.00306931  0.36678436
-#   0.0646545   0.00475969  0.08085762  0.00435456]]
-
+#[[ 0.0399503   0.00846141  0.063187    0.20751727  0.0114865   0.34525406
+#   0.01489209  0.02377231  0.27250814  0.01297088]]
